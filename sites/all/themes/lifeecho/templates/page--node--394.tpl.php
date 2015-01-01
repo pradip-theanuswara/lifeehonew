d8733ff62b97d0d2568f31aa9376b56bfed5383e
pradip-theanuswara <pradip@theanuswara.com>
2015-01-01 23:56:07 +0530

    Update page--node--394.tpl.php

--- sites/all/themes/lifeecho/templates/page--node--394.tpl.php
+++ sites/all/themes/lifeecho/templates/page--node--394.tpl.php
@@ -5,126 +5,78 @@
  * Bartik's theme implementation to display a single Drupal page.
  *
  * The doctype, html, head and body tags are not in this template. Instead they
- * 
-
-can be found in the html.tpl.php template normally located in the
+ * can be found in the html.tpl.php template normally located in the
  * modules/system directory.
  *
  * Available variables:
  *
  * General utility variables:
- * - $base_path: 
-
-The base URL path of the Drupal installation. At the very
+ * - $base_path: The base URL path of the Drupal installation. At the very
  *   least, this will always default to /.
- * - $directory: The directory the template is located in, e.g. 
-
-modules/system
+ * - $directory: The directory the template is located in, e.g. modules/system
  *   or themes/lifeecho.
  * - $is_front: TRUE if the current page is the front page.
  * - $logged_in: TRUE if the user is registered and signed in.
- * - 
-
-$is_admin: TRUE if the user has permission to access administration pages.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
  *
  * Site identity:
- * - $front_page: The URL of the front page. Use this instead of 
-
-$base_path,
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
  *   when linking to the front page. This includes the language domain or
  *   prefix.
- * - $logo: The path to the logo image, as defined in theme 
-
-configuration.
+ * - $logo: The path to the logo image, as defined in theme configuration.
  * - $site_name: The name of the site, empty when display has been disabled
  *   in theme settings.
- * - $site_slogan: The slogan of the site, empty when 
-
-display has been disabled
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
  *   in theme settings.
  * - $hide_site_name: TRUE if the site name has been toggled off on the theme
- *   settings page. If hidden, the 
-
-"element-invisible" class is added to make
+ *   settings page. If hidden, the "element-invisible" class is added to make
  *   the site name visually hidden, but still accessible.
- * - $hide_site_slogan: TRUE if the site slogan has been toggled off 
-
-on the
+ * - $hide_site_slogan: TRUE if the site slogan has been toggled off on the
  *   theme settings page. If hidden, the "element-invisible" class is added to
  *   make the site slogan visually hidden, but still accessible.
  *
  * Navigation:
- * 
-
-- $main_menu (array): An array containing the Main menu links for the
+ * - $main_menu (array): An array containing the Main menu links for the
  *   site, if they have been configured.
- * - $secondary_menu (array): An array containing the 
-
-Secondary menu links for
+ * - $secondary_menu (array): An array containing the Secondary menu links for
  *   the site, if they have been configured.
  * - $breadcrumb: The breadcrumb trail for the current page.
  *
- * Page content (in order of 
-
-occurrence in the default page.tpl.php):
+ * Page content (in order of occurrence in the default page.tpl.php):
  * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front 
-
-of the main title tag that
+ *   modules, intended to be displayed in front of the main title tag that
  *   appears in the template.
  * - $title: The page title, for use in the actual HTML content.
- * - $title_suffix (array): An array containing 
-
-additional output populated by
+ * - $title_suffix (array): An array containing additional output populated by
  *   modules, intended to be displayed after the main title tag that appears in
  *   the template.
- * - $messages: HTML for status and 
-
-error messages. Should be displayed
+ * - $messages: HTML for status and error messages. Should be displayed
  *   prominently.
  * - $tabs (array): Tabs linking to any sub-pages beneath the current page
- *   (e.g., the view and edit tabs when 
-
-displaying a node).
+ *   (e.g., the view and edit tabs when displaying a node).
  * - $action_links (array): Actions local to the page, such as 'Add menu' on the
  *   menu administration interface.
- * - $feed_icons: A string of all 
-
-feed icons for the current page.
+ * - $feed_icons: A string of all feed icons for the current page.
  * - $node: The node object, if there is an automatically-loaded node
- *   associated with the page, and the node ID is the second 
-
-argument
+ *   associated with the page, and the node ID is the second argument
  *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
  *   comment/reply/12345).
  *
  * Regions:
- * - $page['header']: Items for the header 
-
-region.
+ * - $page['header']: Items for the header region.
  * - $page['featured']: Items for the featured region.
  * - $page['highlighted']: Items for the highlighted content region.
- * - $page['help']: Dynamic help text, 
-
-mostly for admin pages.
+ * - $page['help']: Dynamic help text, mostly for admin pages.
  * - $page['content']: The main content of the current page.
  * - $page['sidebar_first']: Items for the first sidebar.
- * - $page
-
-['triptych_first']: Items for the first triptych.
+ * - $page['triptych_first']: Items for the first triptych.
  * - $page['triptych_middle']: Items for the middle triptych.
- * - $page['triptych_last']: Items for the last 
-
-triptych.
+ * - $page['triptych_last']: Items for the last triptych.
  * - $page['footer_firstcolumn']: Items for the first footer column.
  * - $page['footer_secondcolumn']: Items for the second footer column.
- * - $page
-
-['footer_thirdcolumn']: Items for the third footer column.
+ * - $page['footer_thirdcolumn']: Items for the third footer column.
  * - $page['footer_fourthcolumn']: Items for the fourth footer column.
- * - $page['footer']: Items for the 
-
-footer region.
+ * - $page['footer']: Items for the footer region.
  *
  * @see template_preprocess()
  * @see template_preprocess_page()
@@ -135,9 +87,7 @@ footer region.
 
 ?>
 
-<div 
-
-id="MPOuterMost">
+<div id="MPOuterMost">
 <div id="MPOuter">
 <div class="header"></div>
 <div id="menu">
@@ -146,13 +96,9 @@ $beta =  "/sites/all/themes/lifeecho/beta.png";
 ?>
         
 
-<div 
-
-class="logo"> <?php if ($logo): ?>
+<div class="logo"> <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print 
-
-$logo; ?>" alt="<?php print t('Home'); ?>" />
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
       </a>
     <?php endif; ?></div>
 <ul>
@@ -160,81 +106,52 @@ $logo; ?>" alt="<?php print t('Home'); ?>" />
 
     <?php if ($main_menu): ?>
       <li>
-        <?php print theme
-
-('links__system_main_menu', array(
+        <?php print theme('links__system_main_menu', array(
           'links' => $main_menu,
           'attributes' => array(
             'id' => '',
             'class' => array('', ''),
-          
-
-),
+          ),
           'heading' => array(
             'text' => t('Main menu'),
             'level' => 'h2',
             'class' => array('element-invisible'),
           ),
-        
-
-)); ?>
+        )); ?>
       </li> <!-- /#main-menu -->
     <?php endif; ?>
 
    
 </ul>
 </div>
-<div class="second">
 <div class="inner1communitybanner">              
-     <div class="thumb-
-
-container">
+     <div class="thumb-container">
 <?php print render($page['userprofilemybio']); ?>
 </div>    
         <?php if($user->uid != 0 && arg(2) != 'edit') { ?>
 		<ul id="main-nav">
 
-<?php 
-
-//print render($page['userprofile_onpagemenu']); ?>
+<?php //print render($page['userprofile_onpagemenu']); ?>
 
 <div class="region region-userprofile-onpagemenu">
     <div id="block-nice-menus-1" class="block block-nice-menus">
 <div class="content">
     <ul class="nice-menu nice-menu-down" id="nice-menu-1">
 
-<li class="menu-2767 menuparent  menu-path-front   even  home"><a  title="Create" <?php 
-
-if(check_miniprofile_status() == FALSE ) { ?>style="background-position: center top;" <?php } ?>><?php print t('Create'); ?></a><ul><li class="menu-2771 menu-path-user 
-
- first   odd  "><a href="<?php echo url('user/'.$user->uid.'/edit'); ?>" title="Complete profile"><?php print t('Complete Profile'); ?></a></li>
-<li class="menu-2772 
-
-menu-path-front   even   last "><a href="<?php echo url('user/'.$user->uid.'/edit'); ?>" title="Edit Profile"><?php print t('Edit Profile'); ?></a></li>
+<li class="menu-2767 menuparent  menu-path-front   even  home"><a  title="Create" <?php if(check_miniprofile_status() == FALSE ) { ?>style="background-position: center top;" <?php } ?>><?php print t('Create'); ?></a><ul><li class="menu-2771 menu-path-user  first   odd  "><a href="<?php echo url('user/'.$user->uid.'/edit'); ?>" title="Complete profile"><?php print t('Complete Profile'); ?></a></li>
+<li class="menu-2772 menu-path-front   even   last "><a href="<?php echo url('user/'.$user->uid.'/edit'); ?>" title="Edit Profile"><?php print t('Edit Profile'); ?></a></li>
 </ul></li>
 
-<li 
-
-class="menu-2768 menuparent  menu-path-front  first   odd  "><a href="<?php echo base_path(); ?>" title="Connect" <?php if(count(get_logged_user_joined_community_ID()) 
-
-== 0) { ?>style="background-position: center top;" <?php } ?>><?php print t('Connect'); ?></a><ul>
-<?php if($community_count > 0) { // check logged user has a community 
-
-*/ ?>
+<li class="menu-2768 menuparent  menu-path-front  first   odd  "><a href="<?php echo base_path(); ?>" title="Connect" <?php if(count(get_logged_user_joined_community_ID()) == 0) { ?>style="background-position: center top;" <?php } ?>><?php print t('Connect'); ?></a><ul>
+<?php if($community_count > 0) { // check logged user has a community */ ?>
 <li class="menu-2773 menu-path-community-dashboard  first odd"><a href="#" title="My Community"><?php print t('My Community'); ?></a></li>
-<ul id="child-
-
-community-display" style="display: block; visibility:visible">
+<ul id="child-community-display" style="display: block; visibility:visible">
 <?php $community_array = get_logged_user_joined_community_ID();
 if(count($community_array) > 0) {
-for
-
-($i=0;$i<count($community_array);$i++) {
+for($i=0;$i<count($community_array);$i++) {
 $pieces = explode('/',$community_array[$i]);
 ?>
-<li><a href="<?php echo url('node/'.$pieces[0]).'?sparam=1'; ?>" title="My 
-
-Community"><?php 
+<li><a href="<?php echo url('node/'.$pieces[0]).'?sparam=1'; ?>" title="My Community"><?php 
 if(get_logged_user_community_ID() == $pieces[0])
 echo $pieces[1].' (created) ';
 else
@@ -244,20 +161,14 @@ echo $pieces[1];
 } } ?>
 </ul>
 <?php
-} // END community 
-
-count check
+} // END community count check
 ?>
-<li class="menu-2774 menu-path-user-matches   even  "><a href="<?php echo url('userlist/matches'); ?>" title="My Matches"><?php print t('My Matches'); ?
-
-></a></li>
+<li class="menu-2774 menu-path-user-matches   even  "><a href="<?php echo url('userlist/matches'); ?>" title="My Matches"><?php print t('My Matches'); ?></a></li>
 <li class="menu-2775 menu-path-searches   odd  ">
 <?php
 if(count($community_array) > 0) {
 ?>
-<a href="<?php echo url('searches'); ?>" title="Search"><?php print 
-
-t('Search'); ?></a>
+<a href="<?php echo url('searches'); ?>" title="Search"><?php print t('Search'); ?></a>
 <?php
 }else{
 ?>
@@ -266,36 +177,18 @@ t('Search'); ?></a>
 }
 ?>
 </li>
-<li class="menu-2776 
-
-menu-path-joincommunity   even  "><a href="<?php echo url('joincommunity'); ?>" title="Join a Community"><?php print t('Join a Community'); ?></a></li>
-<li 
-
-class="menu-2777 menu-path-node-add-community-lifeecho   odd   last "><a href="<?php echo url('node/add/community-lifeecho'); ?>" title="Create a Community"><?php 
-
-print t('Create a Community'); ?></a></li>
+<li class="menu-2776 menu-path-joincommunity   even  "><a href="<?php echo url('joincommunity'); ?>" title="Join a Community"><?php print t('Join a Community'); ?></a></li>
+<li class="menu-2777 menu-path-node-add-community-lifeecho   odd   last "><a href="<?php echo url('node/add/community-lifeecho'); ?>" title="Create a Community"><?php print t('Create a Community'); ?></a></li>
 </ul></li>
 
-<li class="menu-2769 menuparent  menu-path-front   even   last "><a href="<?php echo base_path(); ?>" title="Equip" 
-
-<?php if(count(get_logged_user_joined_community_ID()) == 1) { ?>style="background-position: center top;" <?php } ?>><?php print t('Equip'); ?></a><ul><li class="menu-
-
-2778 menu-path-user  first   odd  "><a href="<?php echo url('discipleship-101'); ?>" title="Discipleship Training Center"><?php print t('Discipleship Training 
-
-Center'); ?></a></li>
-<li class="menu-2779 menu-path-user even last "><a target="_blank" href="https://www.youversion.com/" title="Bible"><?php print t('Bible'); ?
-
-></a></li>
+<li class="menu-2769 menuparent  menu-path-front   even   last "><a href="<?php echo base_path(); ?>" title="Equip" <?php if(count(get_logged_user_joined_community_ID()) == 1) { ?>style="background-position: center top;" <?php } ?>><?php print t('Equip'); ?></a><ul><li class="menu-2778 menu-path-user  first   odd  "><a href="<?php echo url('discipleship-101'); ?>" title="Discipleship Training Center"><?php print t('Discipleship Training Center'); ?></a></li>
+<li class="menu-2779 menu-path-user even last "><a target="_blank" href="https://www.youversion.com/" title="Bible"><?php print t('Bible'); ?></a></li>
 </ul></li>
 
-<li class="menu-2770 menuparent  menu-path-front odd"><a title="Disciple"><?php print t('Disciple'); ?></a><ul><li class="menu-2780 menu-path-user  
-
-first   even   ">
+<li class="menu-2770 menuparent  menu-path-front odd"><a title="Disciple"><?php print t('Disciple'); ?></a><ul><li class="menu-2780 menu-path-user  first   even   ">
 <a href="https://biblestudy.lifeecho.com" target="_blank" title="Goes to Curriculum by Downline"><?php print t('Curriculum by Downline'); ?></a>
 </li>
-<li class="menu-2780 menu-path-user  first   odd   last "><a href="<?php echo url('calendar'); ?>" title="Goes to main calendar"><?php print t('Goes to main 
-
-calendar'); ?></a></li>
+<li class="menu-2780 menu-path-user  first   odd   last "><a href="<?php echo url('calendar'); ?>" title="Goes to main calendar"><?php print t('Goes to main calendar'); ?></a></li>
 </ul></li>
 
 </ul>
@@ -307,86 +200,60 @@ calendar'); ?></a></li>
 		
 
             </div>
-<div class="innerPagecontentdiv" 
-
-style="width:963px;">
+<div class="innerPagecontentdiv">
       <?php if ($messages): ?>
     <div id="messages"><div class="section clearfix">
       <?php print $messages; ?>
-    </div></div> <!-- /.section, 
-
-/#messages -->
+    </div></div> <!-- /.section, /#messages -->
   <?php endif; ?>
     <div class="contentinner">
 <div class="section">
       
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
-      
-
-<?php if ($title): ?>
+      <?php if ($title): ?>
         <h1 class="title" id="page-title">
           <?php print $title; ?>
         </h1>
       <?php endif; ?>
-      <?php print render
-
-($title_suffix); ?>
+      <?php print render($title_suffix); ?>
       <?php if ($tabs): ?>
         <div class="tabs">
           <?php print render($tabs); ?>
         </div>
       <?php endif; ?>
-      <?php print 
-
-render($page['help']); ?>
+      <?php print render($page['help']); ?>
       <?php if ($action_links): ?>
         <ul class="action-links">
           <?php print render($action_links); ?>
         </ul>
-      <?php 
-
-endif; ?>
+      <?php endif; ?>
       <?php //print render($page['content']); ?>
 	<form action="https://secure.tnbcigateway.com/cart/cart.php" method="POST">
-	<input type="hidden" 
-
-name="key_id" value="3643596" />
+	<input type="hidden" name="key_id" value="3643596" />
 	<input type="hidden" name="action" value="process_variable" />
-	<input type="hidden" name="order_description" value="Donate" 
-
-/>
+	<input type="hidden" name="order_description" value="Donate" />
 	<input type="hidden" name="language" value="en" />
 	<input type="hidden" name="url_finish" value="www.lifeecho.com" />
-	<input type="hidden" 
-
-name="customer_receipt" value="true" />
+	<input type="hidden" name="customer_receipt" value="true" />
 	<input type="hidden" name="hash" value="action|order_description|1226785df4036f4c0e1c36877b79b0a8" />
-	<input type="submit" 
-
-name="submit" value="" class="donate_btn" />
+	<input type="submit" name="submit" value="" class="donate_btn" />
 	</form>
       <?php print $feed_icons; ?>
     </div><!-- /.section, /#content -->
     </div>
 </div>
 
-<div 
-
-class="lifefooter">
-    <div class="qdisc">
-      <?php print render($page['footer']); ?>
-</div>
-</div>	
 <div id="video">
     <div class="footerlist">
-     <?php  print 
-
-render($page['videofooter']); ?>
+     <?php  print render($page['videofooter']); ?>
     </div>
 </div>
 
-  
+  <div class="lifefooter">
+    <div class="qdisc">
+      <?php print render($page['footer']); ?>
+    </div>
 
 
   </div>
